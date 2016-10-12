e30c5bb73ea352f0f3964f4b285ca3dab18534cb
Julia Henderson <yulia.p.henderson@gmail.com>
2016-10-11 22:12:40 -0400

    update modal and login/register buttons style

--- client/components/users/LoginView.jsx
+++ client/components/users/LoginView.jsx
@@ -8,8 +8,8 @@ const propTypes = {
 class LoginView extends React.Component {
   render() {
    return (
-     <div>
-       <button onClick={this.props.openModal} className="login floatRight">Login</button>
+     <div className="login-link">
+       <h3 onClick={this.props.openModal} className="login">Login</h3>
      </div>
    );
  }
