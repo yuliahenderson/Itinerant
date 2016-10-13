367b88b21c96eacef28bbd627f6bf8b9b314694d
Bhavika Desai <desaibh@gmail.com>
2016-10-13 10:56:39 -0400

    working on the API

--- client/components/views/FlightApi.jsx
+++ client/components/views/FlightApi.jsx
@@ -15,54 +15,107 @@ class FlightApi extends React.Component {
     super(props)
     this.state = {
       doors: [],
+      airportDestinationArray: ["ANC", "BRW", "BET", "BTT", "CDV", "DLG", "DUT", "EEK",
+      "FAI", "FYU", "Big", "HOM", "JNU", "ENA", "AKN", "ADQ", "OME", "SDP", "SIT",
+      "SGY", "OOK", "UNK", "VDZ", "WRG", "YAK", "BHM", "DHN", "HSV", "MOB", "MGM",
+      "MSL", "ELD", "XNA", "FSM", "HRO", "LIT", "TXK", "Hll", "FLG", "IGM", "PGA",
+      "PHX", "PRC", "TUS", "YUM", "JPR", "BFL", "BUR", "CLD", "CIC", "IPL", "ACV",
+      "FAT", "IYK", "LGB", "MOD", "MRY", "MRY", "OAK", "ONT", "SNA", "PSP", "RDD",
+      "SMF", "SAN", "SFO", "SJC", "SBP", "SBA", "SMX", "STS", "VIS", "LAX", "YYC",
+      "YEG", "YMM", "YQU", "YQL", "YXH", "YXX", "YAA", "YBL", "YCG", "YXC", "YYE",
+      "YXJ", "YKA", "YLW", "YCD", "YYF", "YZT", "YPW", "YPR", "YQZ", "YZP", "YYD",
+      "YXT", "YVR", "YYJ", "YWL", "YYQ", "YTH", "YWG", "YFC", "YQM", "YSJ", "YDF",
+      "YQX", "YYT", "YWK", "YHZ", "YQY", "YSM", "YZF", "YEK", "YFB", "YRT", "YHD",
+      "YHM", "YQK", "YGK", "YXU", "YYB", "YOW", "YRL", "YZR", "YAM", "YXL", "YSB",
+      "YQT", "YTS", "YYZ", "YTZ", "YQG", "YYG", "YBG", "YGR", "YMT", "YGP", "YYY",
+      "YUL", "YQB", "YRJ", "YUY", "YZV", "YVO", "YQR", "YXE", "YXY", "SPY", "ALS",
+      "ASE", "COS", "DEN", "DRO", "GJT", "GUC", "MTJ", "PUB", "SBS", "EGE", "BDL",
+      "HVN", "DCA", "IAD", "ILG", "QSF", "DAB", "FLL", "RSW", "VPS", "GNV", "JAX",
+      "EYW", "MLB", "MIA", "MCO", "PFN", "PNS", "SRQ", "PIE", "TLH", "TPA", "PBI",
+      "ABY", "AHN", "ATL", "AGS", "BQK", "CSG", "MCN", "SAV", "VLD", "QLA", "YEO",
+      "HNM", "HNL", "MKK", "OGG", "LUP", "JHM", "LNY", "LIH", "BRL", "CID", "DSM",
+      "DBQ", "FOD", "MCW", "SUX", "ALO", "BOI", "IDA", "LWS", "PIH", "SUN", "TWF",
+      "TAX", "BMI", "CMI", "MDW", "ORD", "DEC", "MWA", "MLI", "PIA", "UIN", "RFD",
+      "SPI", "BMG", "CLU", "EVV", "FWA", "IND", "SBN", "SRY", "ABF", "DDC", "GCK",
+      "GBD", "HYS", "LBL", "MHK", "SLN", "ICT", "CVG", "LEX", "SDF", "OWB", "PAH",
+      "AEX", "BTR", "LFT", "LCH", "MLU", "MSY", "SHV", "BED", "BOS", "HYA", "MVY",
+      "ACK", "PVC", "CEF", "BWI", "HGR", "AUG", "BGR", "BHB", "LEW", "PWM", "PQI",
+      "RKD", "APN", "DTW", "ESC", "FNT", "GRR", "CMX", "IMT", "IWD", "AZO", "LAN",
+      "MBL", "MQT", "MKG", "PLN", "MBS", "CIU", "TVC", "BJI", "DLH", "HIB", "INL",
+      "MSP", "RST", "STC", "TVF", "CGI", "COU", "TBN", "JLN", "MCI", "IRK", "SGF",
+      "STL", "GTR", "GLH", "GPT", "JAN", "PIB", "MEI", "TUP", "BIL", "BZN", "BTM",
+      "GDV", "GTF", "HVR", "HLN", "FCA", "LWT", "MLS", "MSO", "SDY", "WYS", "OLF",
+      "AVL", "CLT", "FAY", "GSO", "PGV", "OAJ", "EWN", "RDU", "ILM", "BIS", "DVL",
+      "DIK", "FAR", "GFK", "JMS", "MOT", "ISN", "AIA", "GRI", "EAR", "LNK", "MCK",
+      "LBF", "OMA", "BFF", "MHT", "PSM", "ACY", "EWR", "TTN", "ABQ", "CNM", "CVN",
+      "FMN", "HOB", "LRU", "ROW", "SAF", "SVC", "KTM", "EKO", "LAS", "RNO", "ALB",
+      "BGM", "BUF", "ELM", "ISP", "ITH", "JHW", "MSS", "LGA", "JFK", "SWF", "OGS",
+      "ROC", "SLK", "ART", "HPN", "CAK", "CLE", "CMH", "DAY", "TOL", "WDG", "LAW",
+      "OKC", "TUL", "EUG", "LMT", "MFR", "OTH", "PDT", "PDX", "RDM", "ABE", "AOO",
+      "DUJ", "ERI", "MDT", "JST", "LNS", "LBE", "PHL", "PIT", "SCE", "AVP", "IPT",
+      "LAB", "PVD", "CHS", "CAE", "FLO", "GSP", "HHH", "ABR", "BKX", "HON", "PIR",
+      "RAP", "FSD", "ATY", "CHA", "MKL", "TYS", "MEM", "BNA", "ABI", "AMA", "AUS",
+      "BPT", "BRO", "CLL", "CRP", "DAL", "DFW", "ELP", "HRL", "EFD", "HOU", "IAH",
+      "EFD", "GRK", "LRD", "GGG", "LBB", "MFE", "MAF", "SJT", "SAT", "TYR", "VCT",
+      "ACT", "SPS", "SLC", "SGU", "CHO", "LYH", "PHF", "ORF", "RIC", "ROA", "SHD",
+      "BTV", "RUT", "VSF", "BLI", "PSC", "PUW", "SEA", "GEG", "ALW", "EAT", "YKM",
+      "ATW", "EAU", "GRB", "LSE", "MSN", "MKE", "OSH", "RHI", "CWA", "BKW", "BLF",
+      "CRW", "CKB", "LWB", "HTS", "MGW", "PKB", "CPR", "CYS", "COD", "GCC", "JAC",
+      "LAR", "RIW", "RKS", "SHR", "WRL", "CHI", "CHI", "MWH", "NYC", "OXR", "SOP",
+      "PNC", "TOP", "WAS", "YMQ", "YTO"]
     };
   }
   componentDidMount() {
     this.httpGetFlights();
   }
   httpGetFlights() {
-    const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=LAX&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
-    request.get(url).then((response) => {
-      const budgetData = response.body.offers;
-      let legsIdArray = [];
-      let openFlights = [];
-      if (budgetData) {
-        let budget = Object.keys(budgetData).map((id) => {
-          const individualBudgetData = budgetData[id];
-          let RealLegsId = individualBudgetData.legIds[0].toString();
-          let totalFare = individualBudgetData.totalFare.toString();
-          let detailsURL = individualBudgetData.detailsUrl.toString();
-          const flightData = response.body.legs;
-          if (flightData) {
-          let flights = Object.keys(flightData).map((id) => {
-              const individualFlightData = flightData[id];
-              let legId=individualFlightData.legId;
-              if (individualFlightData.segments.length == 1 &&
-                  individualFlightData.segments[0].hasSeatMap === true &&
-                  RealLegsId === legId) {
-                let airlineName = individualFlightData.segments[0].airlineName;
-                let arrivalAirportCode = individualFlightData.segments[0].arrivalAirportCode;
-                let arrivalAirportLocation = individualFlightData.segments[0].arrivalAirportLocation;
-                let arrivalTime = individualFlightData.segments[0].arrivalTime;
-                let departureAirportCode = individualFlightData.segments[0].departureAirportCode;
-                let departureAirportLocation = individualFlightData.segments[0].departureAirportLocation;
-                let departureTime = individualFlightData.segments[0].departureTime;
-                let flightNumber = individualFlightData.segments[0].flightNumber;
-                openFlights.push({legId, airlineName, arrivalAirportCode, arrivalAirportLocation, arrivalTime,
-                departureAirportCode, departureTime, departureAirportLocation, flightNumber, RealLegsId,
-                totalFare, detailsURL});
-              }
-            });
-            this.setState({
-              doors: openFlights,
-            });
-          }
-        });
-      }
-    })
+    for (let i = 1; i <= 3; i++) {
+      let randomVariable = Math.floor(Math.random()*519);
+      let destinationAirportCode = this.state.airportDestinationArray[randomVariable];
+          const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=${destinationAirportCode}&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
+          request.get(url).then((response) => {
+            const budgetData = response.body.offers;
+            let legsIdArray = [];
+            let openFlights = [];
+            if (budgetData) {
+              let budget = Object.keys(budgetData).map((id) => {
+                const individualBudgetData = budgetData[id];
+                let RealLegsId = individualBudgetData.legIds[0].toString();
+                let totalFare = individualBudgetData.totalFare.toString();
+                let detailsURL = individualBudgetData.detailsUrl.toString();
+                const flightData = response.body.legs;
+                if (flightData) {
+                let flights = Object.keys(flightData).map((id) => {
+                    const individualFlightData = flightData[id];
+                    let legId=individualFlightData.legId;
+                    if (individualFlightData.segments.length == 1 &&
+                        individualFlightData.segments[0].hasSeatMap === true &&
+                        RealLegsId === legId) {
+                      let airlineName = individualFlightData.segments[0].airlineName;
+                      let arrivalAirportCode = individualFlightData.segments[0].arrivalAirportCode;
+                      let arrivalAirportLocation = individualFlightData.segments[0].arrivalAirportLocation;
+                      let arrivalTime = individualFlightData.segments[0].arrivalTime;
+                      let departureAirportCode = individualFlightData.segments[0].departureAirportCode;
+                      let departureAirportLocation = individualFlightData.segments[0].departureAirportLocation;
+                      let departureTime = individualFlightData.segments[0].departureTime;
+                      let flightNumber = individualFlightData.segments[0].flightNumber;
+                      openFlights.push({legId, airlineName, arrivalAirportCode, arrivalAirportLocation, arrivalTime,
+                      departureAirportCode, departureTime, departureAirportLocation, flightNumber, RealLegsId,
+                      totalFare, detailsURL, destinationAirportCode});
+                    }
+                  });
+                  this.setState({
+                    doors: openFlights,
+                  });
+                  console.log(this.state.doors)
+                }
+              });
+            }
+          })
+        }
   }
   httpGetReturnFlights() {
-    const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=LAX&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
+    const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=${destinationAirportCode}&departureDate=${this.props.dateFrom}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
     request.get(url).then((response) => {
       const budgetData = response.body.offers;
       let legsIdArrayReturn = [];
@@ -105,7 +158,6 @@ class FlightApi extends React.Component {
   }
   render() {
     // if (this.props.moneyToSpend > (parseInt(totalFare) + parseInt(returnTotalFare))) {
-
     const value = this.state.doors.map((door) => {
           if (door.legId !== "null" ) {
             return (
@@ -123,10 +175,10 @@ class FlightApi extends React.Component {
                    totalFare = {door.totalFare}
                    detailsURL = {door.detailsURL}
                    returnFlight = {this.httpGetReturnFlights}
+                   destinationAirportCode = {door.destinationAirportCode}
                 />
               )
           }
-
       });
     return(
         <div>
