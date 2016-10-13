42b49aeee495ae421aac59784eb3c3babde96463
Bhavika Desai <desaibh@gmail.com>
2016-10-13 16:40:53 -0400

    data

--- client/components/views/FlightApi.jsx
+++ client/components/views/FlightApi.jsx
@@ -76,45 +76,45 @@ class FlightApi extends React.Component {
       let randomVariable = Math.floor(Math.random()*519);
       let destinationAirportCode = this.state.airportDestinationArray[randomVariable];
       console.log(destinationAirportCode);
-    const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=${destinationAirportCode}&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
-    request.get(url).then((response) => {
-      const budgetData = response.body.offers;
-      if (budgetData) {
-        let budget = Object.keys(budgetData).map((id) => {
-          const individualBudgetData = budgetData[id];
-          let RealLegsId = individualBudgetData.legIds[0].toString();
-          let totalFare = individualBudgetData.totalFare.toString();
-          let detailsURL = individualBudgetData.detailsUrl.toString();
-          const flightData = response.body.legs;
-          if (flightData) {
-          let flights = Object.keys(flightData).map((id) => {
-              const individualFlightData = flightData[id];
-              let legId=individualFlightData.legId;
-              if (individualFlightData.segments.length == 1 &&
-                  individualFlightData.segments[0].hasSeatMap === true &&
-                  RealLegsId === legId) {
-                let airlineName = individualFlightData.segments[0].airlineName;
-                let arrivalAirportLocation = individualFlightData.segments[0].arrivalAirportLocation;
-                let arrivalTime = individualFlightData.segments[0].arrivalTime;
-                let departureAirportCode = individualFlightData.segments[0].departureAirportCode;
-                let departureAirportLocation = individualFlightData.segments[0].departureAirportLocation;
-                let departureTime = individualFlightData.segments[0].departureTime;
-                let flightNumber = individualFlightData.segments[0].flightNumber;
-                openFlights.push({legId, airlineName, destinationAirportCode, arrivalAirportLocation, arrivalTime,
-                departureAirportCode, departureTime, departureAirportLocation, flightNumber, RealLegsId,
-                totalFare, detailsURL});
-                console.log(openFlights)
-              }
-            });
-                this.setState({
-               doors: openFlights,
-            });
-            this.render();
+      const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=${destinationAirportCode}&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
+      request.get(url).then((response) => {
+        const budgetData = response.body.offers;
+        if (budgetData) {
+          let budget = Object.keys(budgetData).map((id) => {
+            const individualBudgetData = budgetData[id];
+            let RealLegsId = individualBudgetData.legIds[0].toString();
+            let totalFare = individualBudgetData.totalFare.toString();
+            let detailsURL = individualBudgetData.detailsUrl.toString();
+            const flightData = response.body.legs;
+            if (flightData) {
+              let flights = Object.keys(flightData).map((id) => {
+                const individualFlightData = flightData[id];
+                let legId=individualFlightData.legId;
+                if (individualFlightData.segments.length == 1 &&
+                    individualFlightData.segments[0].hasSeatMap === true &&
+                    RealLegsId === legId) {
+                  let airlineName = individualFlightData.segments[0].airlineName;
+                  let arrivalAirportLocation = individualFlightData.segments[0].arrivalAirportLocation;
+                  let arrivalTime = individualFlightData.segments[0].arrivalTime;
+                  let departureAirportCode = individualFlightData.segments[0].departureAirportCode;
+                  let departureAirportLocation = individualFlightData.segments[0].departureAirportLocation;
+                  let departureTime = individualFlightData.segments[0].departureTime;
+                  let flightNumber = individualFlightData.segments[0].flightNumber;
+                  openFlights.push({legId, airlineName, destinationAirportCode, arrivalAirportLocation, arrivalTime,
+                  departureAirportCode, departureTime, departureAirportLocation, flightNumber, RealLegsId,
+                  totalFare, detailsURL});
+                  console.log(openFlights)
+                }
+              });
+              this.setState({
+                doors: openFlights,
+              });
+              this.render();
+            }
+          });
         }
-            });
-          }
-        });
-      }
+      });
+    }
   }
   httpGetReturnFlights() {
     const url = `http://terminal2.expedia.com/x/mflights/search?departureAirport=${this.props.travelFrom}&arrivalAirport=${destinationAirportCode}&departureDate=${this.props.dateTo}&apikey=X4ccWU6YHcmRcc8AowPNxAGgVA8QaZ92`;
@@ -158,8 +158,6 @@ class FlightApi extends React.Component {
     })
   }
   render() {
-    // if (this.props.moneyToSpend > (parseInt(totalFare) + parseInt(returnTotalFare))) {
-
     const value = this.state.doors.map((door) => {
           if (door.legId !== "null" ) {
             return (
@@ -177,6 +175,7 @@ class FlightApi extends React.Component {
                    totalFare = {door.totalFare}
                    detailsURL = {door.detailsURL}
                    returnFlight = {this.httpGetReturnFlights}
+                   doorsReturn = {this.state.openFlightsReturn}
                 />
               )
           }
