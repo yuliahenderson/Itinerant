e0385acaaa86af38085c1a150cffeb4b2289ba19
Julia Henderson <yulia.p.henderson@gmail.com>
2016-10-13 09:45:48 -0400

    delete about section from homepage

--- client/components/views/HomePageView.jsx
+++ client/components/views/HomePageView.jsx
@@ -8,14 +8,6 @@ class HomePageView extends React.Component {
       <div>
        <FlightSearch handleResultsView = {this.props.handleResultsView} />
       </div>
-      <section id="about_us">
-       <h1>WHY ITINERANT</h1>
-       <p className="text_about">Lorem Ipsum is simply dummy text of
-       the printing and typesetting industry. Lorem Ipsum has been the
-       industry's standard dummy text ever since the 1500s, when an
-       unknown printer took a galley of type and scrambled it to make
-       a type specimen book.</p>
-      </section>
     </div>
   );
  }
